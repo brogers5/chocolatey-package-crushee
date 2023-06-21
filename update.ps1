f833ff067372659b4ea4136ec84385133ebb4de0
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-06-21 18:11:34 -0400

    Reformat script

--- update.ps1
+++ update.ps1
@@ -4,22 +4,22 @@ Import-Module PowerShellForGitHub
 $owner = 'xanderfrangos'
 $repository = 'crushee'
 
-function global:au_BeforeUpdate ($Package)  {
+function global:au_BeforeUpdate ($Package) {
     Set-DescriptionFromReadme -Package $Package -ReadmePath '.\DESCRIPTION.md'
 }
 
-function global:au_AfterUpdate ($Package)  {
+function global:au_AfterUpdate ($Package) {
     
 }
 
 function global:au_SearchReplace {
     @{
         "$($Latest.PackageName).nuspec" = @{
-            "(<packageSourceUrl>)[^<]*(</packageSourceUrl>)" = "`$1https://github.com/brogers5/chocolatey-package-$($Latest.PackageName)/tree/v$($Latest.Version)`$2"
-            "(<licenseUrl>)[^<]*(</licenseUrl>)" = "`$1https://github.com/$($owner)/$($repository)/blob/v$($Latest.SoftwareVersion)/LICENSE`$2"
-            "(<projectSourceUrl>)[^<]*(</projectSourceUrl>)" = "`$1https://github.com/$($owner)/$($repository)/tree/v$($Latest.SoftwareVersion)`$2"
-            "(<releaseNotes>)[^<]*(</releaseNotes>)" = "`$1https://github.com/$($owner)/$($repository)/releases/tag/v$($Latest.SoftwareVersion)`$2"
-            "(<copyright>)[^<]*(</copyright>)" = "`$1Copyright © $((Get-Date).Year) Xander Frangos`$2"
+            "(<packageSourceUrl>)[^<]*(</packageSourceUrl>)"                            = "`$1https://github.com/brogers5/chocolatey-package-$($Latest.PackageName)/tree/v$($Latest.Version)`$2"
+            "(<licenseUrl>)[^<]*(</licenseUrl>)"                                        = "`$1https://github.com/$($owner)/$($repository)/blob/v$($Latest.SoftwareVersion)/LICENSE`$2"
+            "(<projectSourceUrl>)[^<]*(</projectSourceUrl>)"                            = "`$1https://github.com/$($owner)/$($repository)/tree/v$($Latest.SoftwareVersion)`$2"
+            "(<releaseNotes>)[^<]*(</releaseNotes>)"                                    = "`$1https://github.com/$($owner)/$($repository)/releases/tag/v$($Latest.SoftwareVersion)`$2"
+            "(<copyright>)[^<]*(</copyright>)"                                          = "`$1Copyright © $((Get-Date).Year) Xander Frangos`$2"
             "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
         }
     }
@@ -31,7 +31,7 @@ function global:au_GetLatest {
 
     return @{ 
         SoftwareVersion = $version
-        Version = $version #This may change if building a package fix version
+        Version         = $version #This may change if building a package fix version
     }
 }
 
